9f550064e32a1dfa106b3a8a02d7cc9811f990ef
Raivo <raivokasepuu@gmail.com>
2020-10-05 12:11:10 +0300

    5.10

---
+++ Ülesanne_4_1_Suured_tähed.py
@@ -0,0 +1,17 @@
+"""
+Kontrollülesanne 4.1 - Suured tähed (tähtaeg 7. okt.(incl))
+Tähtaeg: kolmapäev, 7. oktoober 2020, 23.59
+Maksimaalne failide arv: 1
+Töö liik: Individuaaltöö
+
+
+Avaldustel/vormidel/lepingutel on inimese nimi (ees- ja perenimi) kirjutatud nii, et esitähed on suured ja teised tähed
+on väiksed. Aga inimesed kirjutavad eesnime ja perenime erinevalt: ainult väikeste tähtedega, ainult suurte tähtedega
+või kasutades nii suuri kui ka väikseid tähti segamini.
+
+Koostada programm, mis
+
+küsib kasutajalt eesnime ja perekonnanime (ühe sisestusena, nii ees- kui perekonnanimi võib koosneda mitmest nimest);
+väljastab nimed nii, et esitähed on suured, teised tähed väiksed.
+"""
+print(input("Sisestage oma ees- ja perekonnanimi").title())
